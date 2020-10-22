eba0bd11ff600a47aaa99813af084974d06388eb
Danil Skichko <meksvinz@gmail.com>
2020-10-22 21:35:55 +0300

    start hc

--- core/hc_vault.py
+++ core/hc_vault.py
@@ -3,5 +3,5 @@ import requests
 def get_vault_token(vault_addr, k8s_role, auth_path='kubernetes'):
     with open('/var/run/secrets/kubernetes.io/serviceaccount/token', 'r') as token_file:
         jwt_token = token_file.read()
-    token_responce = requests.post(f'http://172.17.0.3/auth/kubernetes/login', data='{role: {}, jwt: {}}'.format(k8s_role, jwt_token))
+    token_responce = requests.post('http://172.17.0.3/auth/kubernetes/login', data={'role': k8s_role, 'j2t': jwt_token})
     return token_responce
