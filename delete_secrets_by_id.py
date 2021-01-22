afb7c51b83db6080321d596cc540c8b00cc8baa6
Danil <info@google.com>
2021-01-22 12:59:03 +0200

    pre-delete hook + delete_secrets_by_id fix

--- delete_secrets_by_id.py
+++ delete_secrets_by_id.py
@@ -3,16 +3,20 @@ from kubernetes import config
 from core.helpers import get_pod_namespace
 from core.k8s_secret_operator import KubeSecretOperator
 
-config.load_kube_config()
+config.load_incluster_config()
 
 env = os.environ
 
-vault_id = env['VAULT_INJECTOR_ID']
 namespace = get_pod_namespace()
+
+if 'VAULT_INJECTOR_ID' in env.keys():
+    vault_injector_id = env['VAULT_INJECTOR_ID']
+else:
+    vault_injector_id = namespace
 
 secrets_operator = KubeSecretOperator(namespace, check_permissions=False)
 
-injector_secrets = secrets_operator.list_secrets(label_dict={'vault-injector': vault_id})
+injector_secrets = secrets_operator.list_secrets(label_dict={'vault-injector': vault_injector_id})
 secrets_to_delete = tuple(secret.metadata.name for secret in injector_secrets)
 
 for secret in secrets_to_delete:
